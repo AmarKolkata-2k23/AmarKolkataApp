7a4f18d386e760bd8e88f7e87d9645cc1a0b5733
AmarKolkata2k23 <152532537+AmarKolkata2k23@users.noreply.github.com>
2023-12-08 17:33:04 +0530

    Add files via upload

---
+++ Authenticate.jsx
@@ -0,0 +1,27 @@
+<div className="ByGoogle" style={{width: '100%', height: '100%', position: 'relative', background: 'linear-gradient(180deg, #7D2929 0%, rgba(124.51, 41.09, 41.09, 0) 100%)'}}>
+    <img className="Image7" style={{width: 393, height: 400, left: -1, top: 0, position: 'absolute'}} src="https://via.placeholder.com/393x400" />
+    <div className="Rectangle104" style={{width: 393, height: 500, left: -1, top: 400, position: 'absolute', opacity: 0.50, background: 'black', borderRadius: 53}} />
+    <div className="ByGoogle" style={{left: 135, top: 439, position: 'absolute', color: '#FFFBFB', fontSize: 24, fontFamily: 'Poppins', fontWeight: '600', wordWrap: 'break-word'}}>By Google</div>
+    <div className="SignInUsing" style={{width: 163, height: 27, left: 115, top: 685, position: 'absolute', textAlign: 'center', color: 'white', fontSize: 14, fontFamily: 'Poppins', fontWeight: '600', wordWrap: 'break-word'}}> sign in using </div>
+    <div className="FilledFacebook" style={{width: 22.89, height: 70.92, left: 235.11, top: 685.08, position: 'absolute', flexDirection: 'column', justifyContent: 'flex-start', alignItems: 'flex-start', display: 'flex'}}>
+        <div className="Bounds" style={{width: 22.89, height: 70.92, opacity: 0, background: 'rgba(28, 142.78, 248, 0.34)'}}></div>
+    </div>
+    <div className="Group35" style={{width: 150, height: 50, left: 209, top: 720, position: 'absolute'}}>
+        <div className="Rectangle107" style={{width: 150, height: 50, left: 0, top: 0, position: 'absolute', background: 'white', borderRadius: 10, border: '1px black solid'}} />
+        <div className="PhoneNo" style={{width: 91, height: 20, left: 42, top: 11, position: 'absolute', color: 'black', fontSize: 18, fontFamily: 'Poppins', fontWeight: '400', wordWrap: 'break-word'}}>Phone No.</div>
+    </div>
+    <div className="Frame2" style={{left: 22, top: 649, position: 'absolute', justifyContent: 'flex-start', alignItems: 'center', gap: 8, display: 'inline-flex'}}>
+        <div className="Line2" style={{width: 150, height: 0, border: '1px white solid'}}></div>
+        <div className="Or" style={{color: 'white', fontSize: 24, fontFamily: 'Poppins', fontWeight: '400', wordWrap: 'break-word'}}>OR</div>
+        <div className="Line1" style={{width: 150, height: 0, border: '1px white solid'}}></div>
+    </div>
+    <div className="Rectangle144" style={{width: 350, height: 70, left: 22, top: 537, position: 'absolute', background: 'white', boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)', borderRadius: 20, border: '2px black solid'}} />
+    <div className="Ellipse43" style={{width: 50, height: 50, left: 39, top: 547, position: 'absolute', background: '#DADADA', borderRadius: 9999, border: '2px black solid'}} />
+    <img className="Ellipse44" style={{width: 46, height: 46, left: 41, top: 549, position: 'absolute', borderRadius: 9999, backdropFilter: 'blur(50px)'}} src="https://via.placeholder.com/46x46" />
+    <div className="Group37" style={{width: 150, height: 50, left: 34, top: 720, position: 'absolute'}}>
+        <div className="Rectangle107" style={{width: 150, height: 50, left: 0, top: 0, position: 'absolute', background: 'white', borderRadius: 10, border: '1px black solid'}} />
+        <div className="Email" style={{width: 58, height: 20, left: 59, top: 11, position: 'absolute', color: 'black', fontSize: 18, fontFamily: 'Poppins', fontWeight: '400', wordWrap: 'break-word'}}>Email</div>
+        <img className="Icons8Mail481" style={{width: 30, height: 23.12, left: 19, top: 13, position: 'absolute'}} src="https://via.placeholder.com/30x23" />
+    </div>
+    <img className="Image5" style={{width: 249, height: 224, left: 70, top: 81, position: 'absolute'}} src="https://via.placeholder.com/249x224" />
+</div>
---
+++ Otp.jsx
@@ -0,0 +1,24 @@
+<div className="Verf" style={{width: '100%', height: '100%', position: 'relative', background: 'linear-gradient(180deg, #7D2929 0%, rgba(124.51, 41.09, 41.09, 0) 100%)'}}>
+    <img className="Image7" style={{width: 393, height: 498, left: -4, top: 2, position: 'absolute'}} src="https://via.placeholder.com/393x498" />
+    <img className="Image5" style={{width: 249, height: 224, left: 71, top: 97, position: 'absolute'}} src="https://via.placeholder.com/249x224" />
+    <div className="SignInUsing" style={{width: 163, height: 27, left: 115, top: 685, position: 'absolute', textAlign: 'center', color: 'white', fontSize: 14, fontFamily: 'Poppins', fontWeight: '600', wordWrap: 'break-word'}}> sign in using </div>
+    <div className="FilledFacebook" style={{width: 22.89, height: 70.92, left: 235.11, top: 685.08, position: 'absolute', flexDirection: 'column', justifyContent: 'flex-start', alignItems: 'flex-start', display: 'flex'}}>
+        <div className="Bounds" style={{width: 22.89, height: 70.92, opacity: 0, background: 'rgba(28, 142.78, 248, 0.34)'}}></div>
+    </div>
+    <div className="Rectangle104" style={{width: 393, height: 396, left: -1, top: 500, position: 'absolute', opacity: 0.50, background: 'rgba(0, 0, 0, 0.90)', borderRadius: 53}} />
+    <div className="EnterTheOtpSent" style={{width: 309, height: 22, left: 51, top: 588, position: 'absolute', color: '#FFFBFB', fontSize: 18, fontFamily: 'Montserrat', fontWeight: '700', wordWrap: 'break-word'}}>Enter the OTP sent</div>
+    <div className="DidnTReceiveOtp" style={{left: 51, top: 682, position: 'absolute', color: '#FCFCFC', fontSize: 16, fontFamily: 'Montserrat', fontWeight: '600', wordWrap: 'break-word'}}>Didn’t receive OTP? </div>
+    <div className="Resend" style={{left: 217, top: 682, position: 'absolute', color: 'rgba(255, 229.50, 0, 0.89)', fontSize: 15, fontFamily: 'Poppins', fontWeight: '700', wordWrap: 'break-word'}}>RESEND</div>
+    <div className="Frame37" style={{width: 289, height: 56, left: 51, top: 625, position: 'absolute', justifyContent: 'flex-start', alignItems: 'flex-start', gap: 23, display: 'inline-flex'}}>
+        <div className="Rectangle112" style={{width: 55, height: 50, background: 'rgba(252.01, 252.01, 252.01, 0.91)', boxShadow: '0px 20px 20px rgba(153, 205.80, 255, 0.85) inset', borderRadius: 12, border: '1px black solid'}} />
+        <div className="Rectangle105" style={{width: 55, height: 50, opacity: 0.10, borderRadius: 12}} />
+        <div className="Rectangle115" style={{width: 55, height: 50, background: 'rgba(252.01, 252.01, 252.01, 0.91)', boxShadow: '0px 20px 20px rgba(153, 205.80, 255, 0.85) inset', borderRadius: 12, border: '1px black solid'}} />
+        <div className="Rectangle114" style={{width: 55, height: 50, background: 'rgba(252.01, 252.01, 252.01, 0.91)', boxShadow: '0px 20px 20px rgba(153, 205.80, 255, 0.85) inset', borderRadius: 12, border: '1px black solid'}} />
+        <div className="Rectangle113" style={{width: 55, height: 50, background: 'rgba(252.01, 252.01, 252.01, 0.91)', boxShadow: '0px 20px 20px rgba(153, 205.80, 255, 0.85) inset', borderRadius: 12, border: '1px black solid'}} />
+    </div>
+    <div className="Group35" style={{width: 350, height: 50, left: 20, top: 720, position: 'absolute'}}>
+        <div className="Rectangle107" style={{width: 350, height: 50, left: 0, top: 0, position: 'absolute', background: 'white', borderRadius: 20, border: '2px black solid'}} />
+        <div className="VerifyContinue" style={{width: 175, left: 88, top: 11, position: 'absolute', color: 'black', fontSize: 18, fontFamily: 'Poppins', fontWeight: '600', wordWrap: 'break-word'}}>VERIFY & CONTINUE</div>
+    </div>
+    <div className="OtpVerification" style={{left: 98, top: 525, position: 'absolute', color: '#FFFBFB', fontSize: 24, fontFamily: 'Poppins', fontWeight: '600', wordWrap: 'break-word'}}>OTP Verification</div>
+</div>
