9ab9f017f685d5ebc59c28d3b14641e2946c88ba
Harshankit Raj <128153963+Harsh-2410@users.noreply.github.com>
2023-12-13 01:04:12 +0530

    Done

---
+++ Client/Number.jsx
@@ -0,0 +1,35 @@
+<div className="Number" style={{width: '100%', height: '100%', position: 'relative', background: 'linear-gradient(180deg, #7D2929 0%, rgba(124.51, 41.09, 41.09, 0) 100%)'}}>
+    <img className="Image7" style={{width: 393, height: 400, left: -1, top: 0, position: 'absolute'}} src="https://via.placeholder.com/393x400" />
+    <img className="Image5" style={{width: 249, height: 224, left: 71, top: 97, position: 'absolute'}} src="https://via.placeholder.com/249x224" />
+    <div className="Rectangle104" style={{width: 393, height: 500, left: -1, top: 400, position: 'absolute', opacity: 0.50, background: 'rgba(0, 0, 0, 0.90)', borderRadius: 53}} />
+    <div className="EnterYourPhoneNumber" style={{left: 40, top: 439, position: 'absolute', color: '#FFFBFB', fontSize: 24, fontFamily: 'Poppins', fontWeight: '600', wordWrap: 'break-word'}}>Enter your phone Number </div>
+    <div className="YouWillReceiveA4DigitVerificationCode" style={{width: 333, height: 24, left: 38, top: 498, position: 'absolute', color: 'white', fontSize: 16, fontFamily: 'Poppins', fontWeight: '400', wordWrap: 'break-word'}}>You will receive a 4 digit verification code</div>
+    <div className="YouWillReceiveA4DigitVerificationCode" style={{width: 333, height: 24, left: 38, top: 498, position: 'absolute', color: 'white', fontSize: 16, fontFamily: 'Poppins', fontWeight: '400', wordWrap: 'break-word'}}>You will receive a 4 digit verification code</div>
+    <div className="Group34" style={{width: 161, height: 31.65, left: 115, top: 606, position: 'absolute'}}>
+        <div className="Rectangle106" style={{width: 161, height: 31.65, left: 0, top: 0, position: 'absolute', background: '#FFFBFB', boxShadow: '-2px 20px 20px #99CEFF inset', borderRadius: 20}}></div>
+        <div className="GenerateOtp" style={{width: 132, height: 23, left: 24, top: 4, position: 'absolute', color: 'black', fontSize: 16, fontFamily: 'Poppins', fontWeight: '400', wordWrap: 'break-word'}}>GENERATE OTP</div>
+    </div>
+    <div className="SignInUsing" style={{width: 163, height: 27, left: 115, top: 685, position: 'absolute', textAlign: 'center', color: 'white', fontSize: 14, fontFamily: 'Poppins', fontWeight: '600', wordWrap: 'break-word'}}> sign in using </div>
+    <div className="FilledFacebook" style={{width: 22.89, height: 70.92, left: 235.11, top: 685.08, position: 'absolute', flexDirection: 'column', justifyContent: 'flex-start', alignItems: 'flex-start', display: 'flex'}}>
+        <div className="Bounds" style={{width: 22.89, height: 70.92, opacity: 0, background: 'rgba(28, 142.78, 248, 0.34)'}}></div>
+    </div>
+    <div className="Group36" style={{width: 150, height: 50, left: 34, top: 720, position: 'absolute'}}>
+        <div className="Rectangle107" style={{width: 150, height: 50, left: 0, top: 0, position: 'absolute', background: 'white', borderRadius: 10, border: '1px black solid'}} />
+        <div className="Google" style={{width: 68, height: 20, left: 51, top: 11, position: 'absolute', color: 'black', fontSize: 18, fontFamily: 'Poppins', fontWeight: '400', wordWrap: 'break-word'}}>Google</div>
+        <img className="GoogleLogo98082" style={{width: 25, height: 25, left: 20, top: 13, position: 'absolute'}} src="https://via.placeholder.com/25x25" />
+    </div>
+    <div className="Group37" style={{width: 150, height: 50, left: 209, top: 720, position: 'absolute'}}>
+        <div className="Rectangle107" style={{width: 150, height: 50, left: 0, top: 0, position: 'absolute', background: 'white', borderRadius: 10, border: '1px black solid'}} />
+        <div className="Email" style={{width: 58, height: 20, left: 59, top: 11, position: 'absolute', color: 'black', fontSize: 18, fontFamily: 'Poppins', fontWeight: '400', wordWrap: 'break-word'}}>Email</div>
+        <img className="Icons8Mail481" style={{width: 30, height: 23.12, left: 19, top: 13, position: 'absolute'}} src="https://via.placeholder.com/30x23" />
+    </div>
+    <div className="Frame2" style={{left: 22, top: 649, position: 'absolute', justifyContent: 'flex-start', alignItems: 'center', gap: 8, display: 'inline-flex'}}>
+        <div className="Line2" style={{width: 150, height: 0, border: '1px white solid'}}></div>
+        <div className="Or" style={{color: 'white', fontSize: 24, fontFamily: 'Poppins', fontWeight: '400', wordWrap: 'break-word'}}>OR</div>
+        <div className="Line1" style={{width: 150, height: 0, border: '1px white solid'}}></div>
+    </div>
+    <div className="Group35" style={{width: 350, height: 50, left: 22, top: 540, position: 'absolute', boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)'}}>
+        <div className="Rectangle107" style={{width: 350, height: 50, left: 0, top: 0, position: 'absolute', background: 'white', borderRadius: 20, border: '2px black solid'}} />
+        <div className="EnterYourPhoneNumberHere" style={{width: 306, left: 23, top: 12, position: 'absolute', color: 'rgba(0, 0, 0, 0.75)', fontSize: 18, fontFamily: 'Poppins', fontWeight: '400', wordWrap: 'break-word'}}>Enter your  phone number here ....</div>
+    </div>
+</div>
