00fad7a6de1018f6d7263ed42ff1813145dde3d8
PRITAM RAUTH <121458064+prith-olo@users.noreply.github.com>
2024-01-02 12:16:07 +0530

    AK
    Trying

--- Client/FirstPage.jsx
+++
@@ -1,8 +0,0 @@
-<div className="StPage" style={{width: '100%', height: '100%', position: 'relative', background: 'linear-gradient(180deg, rgba(124.51, 41.09, 41.09, 0.75) 0%, rgba(124.51, 41.09, 41.09, 0) 100%)'}}>
-    <img className="Image7" style={{width: 393, height: 844, left: -1, top: 0, position: 'absolute'}} src="https://via.placeholder.com/393x844" />
-    <img className="Image5" style={{width: 249, height: 224, left: 71, top: 97, position: 'absolute'}} src="https://via.placeholder.com/249x224" />
-    <div className="Group35" style={{width: 350, height: 50, left: 20, top: 680, position: 'absolute'}}>
-        <div className="Rectangle107" style={{width: 350, height: 50, left: 0, top: 0, position: 'absolute', background: 'white', borderRadius: 20, border: '2px black solid'}} />
-        <div className="LetSStart" style={{width: 93, left: 129, top: 11, position: 'absolute', color: 'black', fontSize: 18, fontFamily: 'Poppins', fontWeight: '600', wordWrap: 'break-word'}}>Let’s Start </div>
-    </div>
-</div>
